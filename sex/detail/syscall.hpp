e463543ac3aa4b53f0cf6f22d325d273f96774ef
Oleg Shatov <8730199@gmail.com>
2022-08-07 22:30:05 +0300

    Fixed syscall macros and added several other macroses

--- sex/detail/syscall.hpp
+++ sex/detail/syscall.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <sex/detail/conditions.hpp>
 #include <sex/util/panic.hpp>
 #include <sex/util/source_location.hpp>
 
@@ -9,19 +8,28 @@
 #include <string_view>
 #include <cstring>
 
-#define SYSCALL(expr) ::sex::detail::SyscallResult(#expr, expr, ::sex::detail::less(0))
-#define SYSCALL_COND(expr, fail_cond) ::sex::detail::SyscallResult(#expr, expr, fail_cond)
+#define SYSCALL(expr) ::sex::detail::SyscallResult(#expr, expr)
+
+#define SEX_LIKELY(expr) __builtin_expect((expr), 1)
+#define SEX_UNLIKELY(expr) !SEX_LIKELY(!(expr))
+
+#define SEX_ASSERT(expr)                                          \
+do {                                                              \
+  if (SEX_UNLIKELY(!(expr))) {                                    \
+    util::Panic(fmt::format("{:s} is not satisfied", #expr));     \
+  }                                                               \
+} while (0)
+
 
 namespace sex::detail {
 
 auto SyscallResult(
   std::string_view expression,
   auto result,
-  auto fail_condition,
   util::SourceLocation location = util::SourceLocation::Current()) {
   int error_code = errno;  // In case if following lines mess up error code
-  if (fail_condition(result)) {
-    Panic(fmt::format("{:s} failed: {:s}({:d})", expression, strerror(error_code), error_code), location);
+  if (SEX_UNLIKELY(int64_t(result) == -1)) {
+    Panic(fmt::format("{:s} failed: {:s} ({:d})", expression.data(), strerror(error_code), error_code), location);
   }
   return result;
 }
